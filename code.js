4473e6d5129bf41da120570bfce6e341c2f0416a
Knoah Cotto <cotto.knoah@gmail.com>
2020-04-23 16:25:16 -0500

    exercise 6

--- code.js
+++ code.js
@@ -3,21 +3,20 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(input){
-    if (input === "Alex") {
-       return "Hello, Alex!";
-    } else if (input === "Jane") {
-        return "Hello, Jane!";
+// function sayHello(input){
+//     if (input === "Alex") {
+//        return "Hello, Alex!";
+//     } else if (input === "Jane") {
+//         return "Hello, Jane!";
+//     } else {
+//         return "Hello, Pat!";
+//     }
+// }
+
+function sayHello(input) {
+    if (typeof input === "string") {
+        return "Hello, " + input + "!";
     } else {
-        return "Hello, Pat!";
+        return "false";
     }
 }
-
-// function sayHello(input){
-//     if (typeof input === "string") {
-//         return "Hello," + input + " !";
-//     } else {
-//         return false;
-//     }
-
-
