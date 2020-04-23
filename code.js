5946df6d439fcdd3346832b2c50bbf9ccd2ff12b
Knoah Cotto <cotto.knoah@gmail.com>
2020-04-23 16:01:40 -0500

    exercise 4

--- code.js
+++ code.js
@@ -3,8 +3,19 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(Jane){
-    return "Hello, Jane!";
+function sayHello(input){
+    if (input === "Alex") {
+       return "Hello, Alex!";
+    } else {
+        return "Hello, Jane!"
+    }
 }
+
+// function sayHello(input){
+//     if (typeof input === "string") {
+//         return "Hello," + input + " !";
+//     } else {
+//         return false;
+//     }
 
 
--- tests.js
+++ tests.js
@@ -22,9 +22,13 @@ describe('sayHello', function () {
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it("should never return 'undefined' when called", function() {
         expect(sayHello()).not.toBe(undefined);
     });
 });
+
